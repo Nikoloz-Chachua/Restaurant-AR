649284ab2e09cf4f32797633a77fae07973201fd
Nikoloz Chachua <Nikoloz-Chachua@users.noreply.github.com>
2026-07-01 17:36:13 +0400

    Allow uploaded GLBs in table AR

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bl-v87';
+const CACHE_NAME = 'bl-v88';
 
 const SUPABASE_URL  = 'https://lwdpegloznhpcecivhfy.supabase.co';
 const SUPABASE_ANON = 'sb_publishable_65dKKpb-lxOr8JTjdj7yxw_LZzcJp5h';
