243d3f93dd1398e7c23feb98fce8e08cef66532c
Nikoloz Chachua <Nikoloz-Chachua@users.noreply.github.com>
2026-07-03 21:54:59 +0400

    feat: promote AR menu items

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bl-v105';
+const CACHE_NAME = 'bl-v106';
 
 const SUPABASE_URL  = 'https://lwdpegloznhpcecivhfy.supabase.co';
 const SUPABASE_ANON = 'sb_publishable_65dKKpb-lxOr8JTjdj7yxw_LZzcJp5h';
